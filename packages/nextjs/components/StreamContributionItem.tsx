d63d8bff5b8938003fc1eb145a8ad1de4036e50c
Hunter Chang <716376+ChangoMan@users.noreply.github.com>
2025-05-08 09:36:48 -0600

    fix date alignment

--- packages/nextjs/components/StreamContributionItem.tsx
+++ packages/nextjs/components/StreamContributionItem.tsx
@@ -12,7 +12,7 @@ export function StreamContributionItem({ title, description, date, amount }: Str
         <div className="mt-2 px-2 py-1 inline-block bg-primary text-primary-content rounded-lg text-lg">{amount}</div>
       </div>
       <div className="lg:col-span-5">
-        <div className="flex flex-col gap-1 lg:flex-row lg:gap-4 lg:items-center">
+        <div className="flex flex-col gap-1 lg:flex-row lg:gap-4 lg:items-baseline">
           <h3 className="m-0 text-xl lg:text-2xl">{title}</h3>
           <p className="m-0 text-sm lg:text-base">{date}</p>
         </div>
