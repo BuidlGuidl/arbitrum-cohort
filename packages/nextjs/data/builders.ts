838dca9196dd313801e0e8bcb511dfbb9cf3b723
Pablo <55535804+Pabl0cks@users.noreply.github.com>
2025-07-24 17:16:28 +0200

    Set initial roster builders data (#46)

--- packages/nextjs/data/builders.ts
+++ packages/nextjs/data/builders.ts
@@ -1,15 +1,37 @@
 export const buildersData = [
   {
-    address: "0x47c3AD3E9b0ce644726237FB930CbB9651c754a6",
-  },
-  {
     address: "0x5dCb5f4F39Caa6Ca25380cfc42280330b49d3c93",
     x: "damianmarti",
     github: "damianmarti",
   },
   {
-    address: "0x6f5c28a7caef574E9A75770C202A27f76d526d08",
-    x: "hunterhchang",
-    github: "ChangoMan",
+    address: "0xB4F53bd85c00EF22946d24Ae26BC38Ac64F5E7B1",
+    x: "pabl0cks",
+    github: "pabl0cks",
+  },
+  {
+    address: "0x1990a6bCdb13D33463cBA884a1aE6020292523e8",
+    x: "_carletex_",
+    github: "carletex",
+  },
+  {
+    address: "0x1A2d838c4bbd1e73d162d0777d142c1d783Cb831",
+    x: "ShivBhonde",
+    github: "technophile-04",
+  },
+  {
+    address: "0x2Bc096A12C5b37F035180aDeF70EB2B88351e5B8",
+    x: "melanke",
+    github: "melanke",
+  },
+  {
+    address: "0x95E08FA8ac4301acC5b943f860Cd8AC84433e3CF",
+    x: "bhavya_gor",
+    github: "bhavyagor12",
+  },
+  {
+    address: "0x4b7b07D8BAf51975eeAb0E1eb4B481A5aC691ED6",
+    x: "NikolaiLeb",
+    github: "NikolaiL",
   },
 ];
