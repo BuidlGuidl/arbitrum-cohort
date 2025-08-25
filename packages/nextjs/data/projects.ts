d237550f5d848ab38a06516d5239114b8bd6184b
Pablo Alayeto <55535804+Pabl0cks@users.noreply.github.com>
2025-08-25 17:02:40 +0200

    Add DevCreds project

--- packages/nextjs/data/projects.ts
+++ packages/nextjs/data/projects.ts
@@ -6,4 +6,13 @@ type Project = {
   liveUrl: string;
 };
 
-export const projectsData: Project[] = [];
+export const projectsData: Project[] = [
+  {
+    name: "dev-creds",
+    title: "DevCreds",
+    description:
+      "DevCreds is an on-chain developer reputation system on Arbitrum powered by attestations (EAS). Think of it as a trustworthy, verifiable version of LinkedIn endorsements, built for the Web3 world.",
+    githubUrl: "https://github.com/damianmarti/dev-creds/",
+    liveUrl: "",
+  },
+];
